2d65f339548f7fa207398bd7d20a08b37b9c7bae
Yadi Rudiyansah <zolandhole@gmail.com>
2019-01-29 17:31:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/androidlock/Fragments/AllAppFragment.java
@@ -0,0 +1,91 @@
+package com.example.androidlock.Fragments;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.androidlock.Adapter.ApplicationListAdapter;
+import com.example.androidlock.Adapter.GetListOfAppsAsyncTask;
+import com.example.androidlock.Data.AppInfo;
+import com.example.androidlock.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllAppFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    static String requiredAppsType;
+
+    public static AllAppFragment newInstance(String requiredApps) {
+        requiredAppsType = requiredApps;
+        AllAppFragment f = new AllAppFragment();
+        return (f);
+    }
+
+
+    public AllAppFragment() {
+        super();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    ProgressDialog progressDialog;
+    List<AppInfo> list;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_all_apps, container, false);
+
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("Loading Apps ...");
+
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        list = new ArrayList<>();
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new ApplicationListAdapter(list , getActivity(), requiredAppsType);
+        mRecyclerView.setAdapter(mAdapter);
+
+        GetListOfAppsAsyncTask task = new GetListOfAppsAsyncTask(this);
+        task.execute(requiredAppsType);
+
+        return v;
+
+    }
+
+
+    public void showProgressBar() {
+        mRecyclerView.setVisibility(View.INVISIBLE);
+        progressDialog.show();
+
+    }
+
+    public void hideProgressBar(){
+        progressDialog.dismiss();
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    public void updateData(List<AppInfo> list){
+        this.list.clear();
+        this.list.addAll(list);
+        mAdapter.notifyDataSetChanged();
+    }
+}
